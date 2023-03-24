98d5f4e749c6749715ced961bc1834a679bbc624
Nghia Nguyen <nhiep2371@gmail.com>
2023-03-24 09:39:46 +0700

    update

--- SE1611_Group 4_Final Project/Pages/Admin/Invoice/InvoiceRequest.cshtml.cs
+++ SE1611_Group 4_Final Project/Pages/Admin/Invoice/InvoiceRequest.cshtml.cs
@@ -6,12 +6,16 @@ using SE1611_Group_4_Final_Project.Utils;
 
 namespace SE1611_Group_4_Final_Project.Pages.Admin.Invoice
 {
-    public class BookingInvoiceModel : PageModel
+    public class InvoiceRequest : PageModel
     {
         private readonly IRepository<Models.Invoice> _repos;
         public IEnumerable<Models.Invoice> Invoices { get; set; }
         [BindProperty]
         public string query { get; set; }
+        public InvoiceRequest(IRepository<Models.Invoice> repos)
+        {
+            _repos = repos;
+        }
 
         public void OnGet()
         {
@@ -22,14 +26,14 @@ namespace SE1611_Group_4_Final_Project.Pages.Admin.Invoice
         {
             var invoice = _repos.Find(id);
             invoice.Status = (int)Constant.InvoiceStatus.RequirePaid;
-            _repos.Update(invoice);
+            _repos.Update(invoice).Wait();
             OnGet();
         }
         public void OnGetConfirmPaid(Guid id)
         {
             var invoice = _repos.Find(id);
             invoice.Status = (int)Constant.InvoiceStatus.Approved;
-            _repos.Update(invoice);
+            _repos.Update(invoice).Wait();
             OnGet();
         }
     }
